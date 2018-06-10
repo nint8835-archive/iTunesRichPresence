f30b6ff7209a264819d9375e4c4e72da1b4a2af1
Riley Flynn <riley@rileyflynn.me>
2018-06-10 15:27:58 -0230

    Added the ability to start software on startup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Reflection;
 using System.Windows;
 using System.Windows.Forms;
 using iTunesLib;
+using Microsoft.Win32;
+using Application = System.Windows.Application;
 
 namespace iTunesRichPresence_Rewrite {
     /// <summary>
@@ -18,6 +20,8 @@ namespace iTunesRichPresence_Rewrite {
             _iTunes = new iTunesApp();
             _notifyIcon = new NotifyIcon {Text = "iTunesRichPresence", Visible = false, Icon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location)};
             _notifyIcon.MouseDoubleClick += (sender, args) => { WindowState = WindowState.Normal; };
+
+            RunOnStartupCheckbox.IsChecked = Properties.Settings.Default.RunOnStartup;
         }
 
         private void MetroWindow_StateChanged(object sender, System.EventArgs e) {
@@ -32,8 +36,24 @@ namespace iTunesRichPresence_Rewrite {
             }
         }
 
-        private async void DiagnosticsButton_OnClick(object sender, RoutedEventArgs e) {
+        private void DiagnosticsButton_OnClick(object sender, RoutedEventArgs e) {
             throw new NotImplementedException();
+        }
+
+        private void RunOnStartupCheckbox_OnClick(object sender, RoutedEventArgs e) {
+            if (RunOnStartupCheckbox.IsChecked ?? false) {
+                Properties.Settings.Default.RunOnStartup = true;
+                Properties.Settings.Default.Save();
+
+                Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true)?.SetValue("iTunesRichPresence", Assembly.GetExecutingAssembly().Location);
+
+            }
+            else {
+                Properties.Settings.Default.RunOnStartup = false;
+                Properties.Settings.Default.Save();
+
+                Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true)?.DeleteValue("iTunesRichPresence");
+            }
         }
     }
 }
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -22,5 +22,17 @@ namespace iTunesRichPresence_Rewrite.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool RunOnStartup {
+            get {
+                return ((bool)(this["RunOnStartup"]));
+            }
+            set {
+                this["RunOnStartup"] = value;
+            }
+        }
     }
 }
