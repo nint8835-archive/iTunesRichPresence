9b7eabb790bc619a34ffc85f19335ab55ff64030
Riley Flynn <riley@rileyflynn.me>
2018-06-09 22:16:22 -0230

    Made window use mahapps.metro for styling

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,11 +5,20 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MahApps.Metro;
 
 namespace iTunesRichPresence_Rewrite {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+
+            ThemeManager.ChangeAppStyle(Application.Current,
+                                        ThemeManager.GetAccent("Red"),
+                                        ThemeManager.GetAppTheme("BaseDark"));
+
+            base.OnStartup(e);
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace iTunesRichPresence_Rewrite {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow {
         public MainWindow() {
             InitializeComponent();
         }
