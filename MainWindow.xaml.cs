1d6a55f4cc05257cf61b05fc8500e934d66c4e06
Riley Flynn <riley@rileyflynn.me>
2018-06-09 22:36:17 -0230

    Made app minimize to system tray

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace iTunesRichPresence_Rewrite {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
+
+        private readonly NotifyIcon _notifyIcon;
+
         public MainWindow() {
             InitializeComponent();
+            _notifyIcon = new NotifyIcon {Text = "iTunesRichPresence", Visible = false, Icon = System.Drawing.Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location)};
+            _notifyIcon.MouseDoubleClick += (sender, args) => { WindowState = WindowState.Normal; };
+        }
+
+        private void MetroWindow_StateChanged(object sender, System.EventArgs e) {
+            if (WindowState == WindowState.Minimized) {
+                ShowInTaskbar = false;
+                _notifyIcon.Visible = true;
+                _notifyIcon.ShowBalloonTip(5000, "iTunesRichPresence hidden to tray", "iTunesRichPresence has been minimized to the system tray. Double click the icon to show the window again.", ToolTipIcon.None);
+            }
+            else {
+                ShowInTaskbar = true;
+                _notifyIcon.Visible = false;
+            }
         }
     }
 }
