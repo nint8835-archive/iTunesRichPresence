1bacbe70e908682f2ee85ea19593fb40ef58ee57
Riley Flynn <riley@rileyflynn.me>
2018-07-07 20:21:01 -0230

    Added a couple of hidden diagnostic tokens to get details about the number of pushes

---
+++ Tokens/Hidden/PushesToken.cs
@@ -0,0 +1,14 @@
+﻿using iTunesLib;
+
+namespace iTunesRichPresence_Rewrite.Tokens.Hidden {
+    public class PushesToken : IToken {
+        public string DisplayName => "Pushes";
+        public string Token => "%pushes";
+        public bool ShowInToolbox => false;
+        private int _pushes = 0;
+        public string GetText(IiTunes iTunes) {
+            _pushes++;
+            return _pushes.ToString();
+        }
+    }
+}
