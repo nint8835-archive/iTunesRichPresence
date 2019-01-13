1fc76105664b3d2bc13f12545775508e4724c515
Riley Flynn <riley@rileyflynn.me>
2019-01-13 12:11:18 -0330

    Began adding patreon features

---
+++ API/APIConnector.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using iTunesRichPresence_Rewrite.API.Responses;
+
+namespace iTunesRichPresence_Rewrite.API {
+    class APIConnector {
+
+        private const string APIAddress = "http://localhost:8181";
+        private const string PatronStatusEndpoint = APIAddress + "/status";
+        private readonly HttpClient _client = new HttpClient();
+
+        public string emailAddress;
+
+        public APIConnector(string email) {
+            emailAddress = email;
+        }
+
+        public async Task<PatronStatusResponse> getPatronStatus() {
+        }
+
+    }
+}
