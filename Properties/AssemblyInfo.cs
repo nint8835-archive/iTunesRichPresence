4ee9de10bfe4ec73e73a3edfdbc817c1719ee056
Riley Flynn <riley@rileyflynn.me>
2019-03-16 14:00:04 -0230

    Incremented version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("iTunesRichPresence-Rewrite")]
-[assembly: AssemblyCopyright("Copyright © Riley Flynn 2018")]
+[assembly: AssemblyCopyright("Copyright © Riley Flynn 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
