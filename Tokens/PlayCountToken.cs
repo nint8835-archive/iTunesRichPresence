52bc60da3e1fb5edc25fe8f8f775f78bd43c2a2c
Riley Flynn <riley@rileyflynn.me>
2019-03-28 11:58:37 -0230

    Resolves #35. Added play count token

---
+++ Tokens/PlayCountToken.cs
@@ -0,0 +1,13 @@
+﻿using iTunesLib;
+
+namespace iTunesRichPresence_Rewrite.Tokens
+{
+    class PlayCountToken : IToken {
+        public string DisplayName => "Play count";
+        public string Token => "%play_count";
+        public bool ShowInToolbox => true;
+        public string GetText(IiTunes iTunes) {
+            return iTunes.CurrentTrack.PlayedCount.ToString();
+        }
+    }
+}
