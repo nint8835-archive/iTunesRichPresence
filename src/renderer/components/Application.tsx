d07d994fc65943b7062e00969a04c28f7956805f
Riley Flynn <riley@rileyflynn.me>
2020-02-29 17:40:27 -0330

    Slim template

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,12 +1,9 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
-import CounterContainer from '../containers/CounterContainer';
-
 const Application = () => (
     <div>
         Hello World from Electron!
-        <CounterContainer />
     </div>
 );
 
